dafe6f1132ab4d339c56ff962b597d962ca5e5cc
Duana Saskia <duana@soundcloud.com>
2013-07-29 23:46:27 +0200

    Included modules can access instance variables

--- controller.rb
+++ controller.rb
@@ -1,13 +1,19 @@
 module SessionsHelper
   def set_current_user(username)
-    @current_user = username
+    @current_user = username if @admin_only
   end
+
 end
 
 class Controller
   include SessionsHelper
 
+  def initialize(admin_only)
+    @admin_only = admin_only
+  end
+
   def do_something
     puts "current user is #{@current_user}"
   end
+
 end

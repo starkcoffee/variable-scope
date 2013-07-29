a1892d877033a419d4505b691016eab1140e6484
Duana Saskia <duana@soundcloud.com>
2013-07-29 22:03:05 +0200

    Instance variables can defined in any instance method

--- circle.rb
+++ circle.rb
@@ -2,17 +2,21 @@
 class Circle
   PI = 3.14
 
-  @radius = 1
-
   def initialize(r)
+    @radius = r
   end
 
   def area
+    @area_calculated = true
     PI * @radius**2
   end
 
   def circumference
     2 * PI * @radius
+  end
+
+  def area_calculated?
+    @area_calculated
   end
 end
 
